8eed58975a2cd6895f22a308f0fb35544a6ade22
martntarget-crypto <martntarget@gmail.com>
2025-10-10 17:42:36 +0400

    Update bot.py

--- bot.py
+++ bot.py
@@ -21,9 +21,9 @@ from collections import Counter, defaultdict
 
 # ---- Aiogram 3.x Imports ----
 from aiogram import Bot, Dispatcher, types
+from aiogram.filters import Command
+from aiogram.filters.text import Text
 from aiogram.fsm.storage.memory import MemoryStorage
-from aiogram.fsm.state import State, StatesGroup
-from aiogram.fsm.context import FSMContext
 from aiogram.types import (
     ReplyKeyboardMarkup, KeyboardButton,
     InlineKeyboardMarkup, InlineKeyboardButton, CallbackQuery, InputMediaPhoto
